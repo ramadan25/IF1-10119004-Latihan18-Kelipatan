6a9afd310a0c9016ee885f9b003abb714ff7054b
ramadan25 <72897383+ramadan25@users.noreply.github.com>
2020-10-30 22:55:28 +0700

    Add files via upload

---
+++ IF1-10119004-Latihan18-Kelipatan/src/if1/pkg10119004/latihan18/kelipatan/IF110119004Latihan18Kelipatan.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg10119004.latihan18.kelipatan;
+
+
+/**
+ *
+ * @author ENDOG
+ */
+public class IF110119004Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+     public static Double angka;
+    public static void perulangan() {
+        angka = 3.5;
+        for (int i = 1; i <= 10; i++) {
+            System.out.println(i * angka);
+        }
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+         perulangan();  
+    }
+    
+    
+}
